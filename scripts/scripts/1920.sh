dc5ca451f86388e2fa0195e7d5abed0b97345190
B0hrer <martin.bielefeld@hotmail.de>
2021-12-16 15:50:10 +0100

    scrips for handling stretched resolutions

---
+++ scripts/scripts/1920.sh
@@ -0,0 +1,1 @@
+nvidia-settings -a CurrentMetaMode="DVI-D-0: 1920x1080_144 @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: nvidia-auto-select @1920x1080 +1920+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
