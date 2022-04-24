7e8f72dc9363feceafea892f349e6da08dfdb969
B0hrer <martin.bielefeld@hotmail.de>
2022-04-24 09:50:01 +0200

    res scripts for 2nd PC

---
+++ scripts/scripts/1280_acer.sh
@@ -0,0 +1,1 @@
+nvidia-settings -a CurrentMetaMode="HDMI-0: 1920x1080_60 @1280x960 +0+0 {ViewPortIn=1280x960, ViewPortOut=1920x1080+0+0, ForceCompositionPipeline=Off, ForceFullCompositionPipeline=Off}"
